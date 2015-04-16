12724551b9f8c46492afcff9112af51af7b81224
PatriciaAbreu <PatriciaAbreu@MacBook-Pro-de-Patricia.local>
2015-04-16 14:48:24 -0300

    imagem de profile

--- WWDCPatriciaAbreu/WWDCPatriciaAbreu/ViewController.swift
+++ WWDCPatriciaAbreu/WWDCPatriciaAbreu/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileImage.layer.cornerRadius = profileImage.frame.size.width/2.4
+        profileImage.layer.borderWidth = 3
+        profileImage.clipsToBounds = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
